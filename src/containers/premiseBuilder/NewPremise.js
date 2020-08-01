b5c135f90a76b2f7403d54ae6b0fdd67e5b0342a
Aaron Pennington <aaronpennington@Macintosh.local>
2020-08-01 16:57:04 -0600

    added copy for premisebuilder

--- src/containers/premiseBuilder/NewPremise.js
+++ src/containers/premiseBuilder/NewPremise.js
@@ -32,6 +32,10 @@ export default function NewPremise(props) {
     return protagonist.length > 0;
   }
 
+  function allFieldsFilled() {
+    return protagonist.length > 0 & situation.length > 0 & objective.length > 0 & opponent.length > 0 & disaster.length > 0 & theme.length > 0 & dramaticStatement.length > 0 & dramaticQuestion.length > 0;
+  }
+
   // function handleFileChange(event) {
   //   file.current = event.target.files[0];
   // }
@@ -80,7 +84,7 @@ export default function NewPremise(props) {
   }
 
   return (
-    <div className="NewNote">
+    <div className="NewNote text-justify">
       <Breadcrumb>
         <Breadcrumb.Item as="div">
           <Link to="/">Home</Link>
@@ -102,7 +106,7 @@ export default function NewPremise(props) {
           <Collapse className="Lexend-Tera" in={openProtagonist}>
             <div className="pale-silver pb-3">
               <p>
-                The protagonist is the hero(ine) of your story. You can write as little or as much as you like here. Start with just a name if you like and come back later to add a description of vice-versa.
+                The protagonist is the hero(ine) of your story. You can write as little or as much as you like here. Start with just a name if you like and come back later to add a description or vice-versa.
               </p>
             </div>
           </Collapse>
@@ -156,7 +160,7 @@ export default function NewPremise(props) {
           <Collapse className="Lexend-Tera" in={openObjective}>
             <div className="pale-silver">
               <p>
-                The objective is simply the protagonist's desire through out the entire story. What do they want?
+                The objective is simply the protagonist's desire through out the entire story. What do they want? Describe that here.
               </p>
             </div>
           </Collapse>
@@ -183,8 +187,8 @@ export default function NewPremise(props) {
           <Collapse className="Lexend-Tera" in={openOpponent}>
             <div className="pale-silver">
               <p>
-                The bad guy. The villian. The Opponent. Who is in your hero's way? Or or more likely, who is your hero in the way of? In this approach the Opponent can be an act of nature or aspect of the hero themselves. Or just a person. You're the boss. 
-                Describe the Opponent here. 
+                The bad guy. The villian. The Opponent. Who is in your hero's way? Or or more likely, who is your hero in the way of? In this approach the opponent can be an act of nature or aspect of the hero themselves. Or just a person. You're the boss. 
+                Describe the opponent here. 
               </p>
             </div>
           </Collapse>
@@ -241,10 +245,7 @@ export default function NewPremise(props) {
           <Collapse className="Lexend-Tera" in={openOpponent}>
             <div className="pale-silver">
               <p>
-                And finally, the theme. What is your story about? What singular truth must the hero finally accept in order to overcome all odds and win in the end? This could be one word. It probably should be, eventually. To start just start free writing on what you think it is about. 
-              </p>
-              <p>
-                You'll get to that one word in time. That is your theme.
+                And finally, the theme. What is your story about? What singular truth must the hero finally accept in order to overcome all odds and win in the end? This could be one word. It probably should be one word in fact... eventually. To start just start free writing on what you think this story is about until you can distill down to one word or one sentence. 
               </p>
             </div>
           </Collapse>
@@ -268,10 +269,25 @@ export default function NewPremise(props) {
               onClick={() => setOpenDramaticStatement(!openDramaticStatement)} 
             />
           </h3>
-          <Collapse className="Lexend-Tera" in={openOpponent}>
+          <Collapse className="Lexend-Tera" in={openDramaticStatement}>
             <div className="pale-silver">
               <p>
-
+                Now that we have considered all of the elements of a premise, let's start putting them together.
+              </p>
+              <p>
+                The dramatic statment is the first of two parts of the finished premise. In this part, try to establish the protagonist, the situation, and the objective in one or two sentences. You can think of it as an equation if that helps:
+              </p>
+              <p className="text-center dark-goldenrod">
+                [PROTAGONIST] + [SITUATION] + [OBJECTIVE] = Dramatic Statement
+              </p>
+              <p>
+                For the non-math nerd, another perspective on the same thing:
+              </p>
+              <p className="text-center dark-goldenrod">
+                "In a [SITUATION], all [PROTAGONIST] wants is [OBJECTIVE]." &#8212;dramatic statement
+              </p>
+              <p>
+                Or something along those lines.
               </p>
             </div>
           </Collapse>
@@ -295,10 +311,16 @@ export default function NewPremise(props) {
               onClick={() => setOpenDramaticQuestion(!openDramaticQuestion)} 
             />
           </h3>
-          <Collapse className="Lexend-Tera" in={openOpponent}>
+          <Collapse className="Lexend-Tera" in={openDramaticQuestion}>
             <div className="pale-silver">
-              <p className="pale-silver">
-
+              <p>
+                In the dramatic question, try to establish the opponent and the disaster in the form of a question. The fill-in-the-blank example might look something like:
+              </p>
+              <p className="text-center dark-goldenrod">
+                But can [PROTAGONIST] get [OBJECTIVE] before [OPPONENT] causes [DISASTER]?
+              </p>
+              <p>
+                I can already tell I'm going to have to rewrite that one but, for now, you hopefully get the idea.
               </p>
             </div>
           </Collapse>
@@ -319,6 +341,16 @@ export default function NewPremise(props) {
           <ControlLabel>Attachment</ControlLabel>
           <FormControl onChange={handleFileChange} type="file" />
         </FormGroup> */}
+        {!!allFieldsFilled() && 
+          <div className="pale-silver">
+            <h2 className="text-center">Congrats!!!</h2>
+            <div className="Lexend-Tera">
+              <p>
+                If you have gotten this far, you have either come up with a new story idea or you are more comfortable with the process for coming up with one. Now just save what you have and move on to SceneBuilder where you will apply the story structure to your premise. You can also save it and come back to it later if you want. It's up to you!
+              </p>
+            </div>
+          </div>
+        }
         <LoaderButton
           block
           type="submit"
@@ -327,7 +359,7 @@ export default function NewPremise(props) {
           isLoading={isLoading}
           disabled={!validateForm()}
         >
-          Create Premise
+          Save Premise
         </LoaderButton>
       </form>
     </div>
--- src/containers/premiseBuilder/Premises.js
+++ src/containers/premiseBuilder/Premises.js
@@ -1,8 +1,9 @@
 import React, { useRef, useState, useEffect } from "react";
 import { API, Storage } from "aws-amplify";
-import { FormGroup, FormControl, Breadcrumb } from "react-bootstrap";
+import { Collapse, FormGroup, FormControl, Breadcrumb } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import LoaderButton from "../../components/LoaderButton";
+import { GoQuestion } from "react-icons/go";
 import { s3Upload } from "../../libs/awsLib";
 import config from "../../config";
 import "../../containers/Notes.css";
@@ -18,6 +19,14 @@ export default function Premise(props) {
     const [theme, setTheme] = useState("");
     const [dramaticStatement, setDramaticStatement] = useState("");
     const [dramaticQuestion, setDramaticQuestion] = useState("");
+    const [openProtagonist, setOpenProtagonist] = useState(false);
+    const [openSituation, setOpenSituation] = useState(false);
+    const [openObjective, setOpenObjective] = useState(false);
+    const [openOpponent, setOpenOpponent] = useState(false);
+    const [openDisaster, setOpenDisaster] = useState(false);
+    const [openTheme, setOpenTheme] = useState(false);
+    const [openDramaticStatement, setOpenDramaticStatement] = useState(false);
+    const [openDramaticQuestion, setOpenDramaticQuestion] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
     const [isDeleting, setIsDeleting] = useState(false);
 
@@ -161,55 +170,207 @@ export default function Premise(props) {
       {premise && (
         <form onSubmit={handleSubmit}>
         <h2>The Premise</h2>
-          <h3>Your Protagonist</h3>
+          <h3>
+            Your Protagonist
+            <GoQuestion 
+              className="pale-silver float-right" 
+              onClick={() => setOpenProtagonist(!openProtagonist)} 
+            />
+          </h3>
+          <Collapse className="Lexend-Tera" in={openProtagonist}>
+            <div className="pale-silver pb-3">
+              <p>
+                The protagonist is the hero(ine) of your story. You can write as little or as much as you like here. Start with just a name if you like and come back later to add a description or vice-versa.
+              </p>
+            </div>
+          </Collapse>
+          <div className="Lexend-Tera pb-3">
+            {/* eslint-disable-next-line */}
+            <a href="https://guides.nanowritlabs.com/guides/PremiseBuilder/#the-protagonist" target="_blank">Guide</a>
+            {/* eslint-disable-next-line */}
+            <a href="https://workshop.nanowritlabs.com/c/PremiseBuilder/protagonist/" target="_blank" className="float-right">Discuss</a>
+          </div>
             <FormGroup controlId="protagonist">
               <FormControl
                 value={protagonist}
-                type="text"
+                as="textarea"
                 onChange={e => setProtagonist(e.target.value)}
               />
             </FormGroup>
-          <h3>The Situation</h3>
+          <h3>
+            The Situation
+            <GoQuestion 
+              className="pale-silver float-right" 
+              onClick={() => setOpenSituation(!openSituation)} 
+            />
+          </h3>
+          <Collapse className="Lexend-Tera" in={openSituation}>
+            <div className="pale-silver pb-3">
+              <p>
+                The Situation is the context of your story. Context can mean just about anything but try to describe the situation as the environment that your protagonist finds themselves in.
+              </p>
+            </div>
+          </Collapse>
+          <div className="Lexend-Tera pb-3">
+            {/* eslint-disable-next-line */}
+            <a href="https://guides.nanowritlabs.com/guides/PremiseBuilder/#the-situation" target="_blank">Guide</a>
+            {/* eslint-disable-next-line */}
+            <a href="https://workshop.nanowritlabs.com/c/PremiseBuilder/situation/" target="_blank" className="float-right">Discuss</a>
+          </div>
             <FormGroup controlId="situation">
               <FormControl
                 value={situation}
-                componentClass="textarea"
+                as="textarea"
                 onChange={e => setSituation(e.target.value)}
               />
             </FormGroup>
-          <h3>The Protagonist's Objective</h3>
+          <h3>
+            The Protagonist's Objective
+            <GoQuestion 
+              className="pale-silver float-right" 
+              onClick={() => setOpenObjective(!openObjective)} 
+            />
+          </h3>
+          <Collapse className="Lexend-Tera" in={openObjective}>
+            <div className="pale-silver">
+              <p>
+                The objective is simply the protagonist's desire through out the entire story. What do they want? Describe that here.
+              </p>
+            </div>
+          </Collapse>
+          <div className="Lexend-Tera pb-3">
+            {/* eslint-disable-next-line */}
+            <a href="https://guides.nanowritlabs.com/guides/PremiseBuilder/#the-objective" target="_blank">Guide</a>
+            {/* eslint-disable-next-line */}
+            <a href="https://workshop.nanowritlabs.com/c/PremiseBuilder/objective/" target="_blank" className="float-right">Discuss</a>
+          </div>
             <FormGroup controlId="objective">
               <FormControl
                 value={objective}
-                type="text"
+                as="textarea"
                 onChange={e => setObjective(e.target.value)}
               />
             </FormGroup>
-          <h3>The Protagonist's Opponent</h3>
+          <h3>
+            The Protagonist's Opponent
+            <GoQuestion 
+              className="pale-silver float-right" 
+              onClick={() => setOpenOpponent(!openOpponent)} 
+            />
+          </h3>
+          <Collapse className="Lexend-Tera" in={openOpponent}>
+            <div className="pale-silver">
+              <p>
+                The bad guy. The villian. The Opponent. Who is in your hero's way? Or or more likely, who is your hero in the way of? In this approach the opponent can be an act of nature or aspect of the hero themselves. Or just a person. You're the boss. 
+                Describe the opponent here. 
+              </p>
+            </div>
+          </Collapse>
+          <div className="Lexend-Tera pb-3">
+            {/* eslint-disable-next-line */}
+            <a href="https://guides.nanowritlabs.com/guides/PremiseBuilder/#the-opponent" target="_blank">Guide</a>
+            {/* eslint-disable-next-line */}
+            <a href="https://workshop.nanowritlabs.com/c/PremiseBuilder/opponent/" target="_blank" className="float-right">Discuss</a>
+          </div>
             <FormGroup controlId="opponent">
               <FormControl
                 value={opponent}
-                type="text"
+                as="textarea"
                 onChange={e => setOpponent(e.target.value)}
               />
             </FormGroup>
-          <h3>The Disaster that Looms over our Hero</h3>
+          <h3>
+            The Disaster that Looms over our Hero
+            <GoQuestion 
+              className="pale-silver float-right" 
+              onClick={() => setOpenDisaster(!openDisaster)} 
+            />
+          </h3>
+          <Collapse className="Lexend-Tera" in={openDisaster}>
+            <div className="pale-silver">
+              <p>
+                "But what if the hero fails? What if the villian wins?" 
+              </p>
+              <p>
+                Describe that situation here.
+              </p>
+            </div>
+          </Collapse>
+          <div className="Lexend-Tera pb-3">
+            {/* eslint-disable-next-line */}
+            <a href="https://guides.nanowritlabs.com/guides/PremiseBuilder/#the-disaster" target="_blank">Guide</a>
+            {/* eslint-disable-next-line */}
+            <a href="https://workshop.nanowritlabs.com/c/PremiseBuilder/disaster/" target="_blank" className="float-right">Discuss</a>
+          </div>
             <FormGroup controlId="disaster">
               <FormControl
                 value={disaster}
-                componentClass="textarea"
+                as="textarea"
                 onChange={e => setDisaster(e.target.value)}
               />
             </FormGroup>
-          <h3>The Theme of your Story</h3>
+          <h3>
+            The Theme of your Story
+            <GoQuestion 
+              className="pale-silver float-right" 
+              onClick={() => setOpenTheme(!openTheme)} 
+            />
+          </h3>
+          <Collapse className="Lexend-Tera" in={openOpponent}>
+            <div className="pale-silver">
+              <p>
+                And finally, the theme. What is your story about? What singular truth must the hero finally accept in order to overcome all odds and win in the end? This could be one word. It probably should be one word in fact... eventually. To start just start free writing on what you think this story is about until you can distill down to one word or one sentence. 
+              </p>
+            </div>
+          </Collapse>
+          <div className="Lexend-Tera pb-3">
+            {/* eslint-disable-next-line */}
+            <a href="https://guides.nanowritlabs.com/guides/PremiseBuilder/#the-disaster" target="_blank">Guide</a>
+            {/* eslint-disable-next-line */}
+            <a href="https://workshop.nanowritlabs.com/c/PremiseBuilder/disaster/" target="_blank" className="float-right">Discuss</a>
+          </div>
             <FormGroup controlId="theme">
               <FormControl
                 value={theme}
-                componentClass="textarea"
+                as="textarea"
                 onChange={e => setTheme(e.target.value)}
               />
             </FormGroup>
-          <h3>The Dramatic Statement</h3>
+          <h3>
+            The Dramatic Statement
+            <GoQuestion 
+              className="pale-silver float-right" 
+              onClick={() => setOpenDramaticStatement(!openDramaticStatement)} 
+            />
+          </h3>
+          <Collapse className="Lexend-Tera" in={openDramaticStatement}>
+            <div className="pale-silver">
+              <p>
+                Now that we have considered all of the elements of a premise, let's start putting them together.
+              </p>
+              <p>
+                The dramatic statment is the first of two parts of the finished premise. In this part, try to establish the protagonist, the situation, and the objective in one or two sentences. You can think of it as an equation if that helps:
+              </p>
+              <p className="text-center dark-goldenrod">
+                [PROTAGONIST] + [SITUATION] + [OBJECTIVE] = Dramatic Statement
+              </p>
+              <p>
+                For the non-math nerd, another perspective on the same thing:
+              </p>
+              <p className="text-center dark-goldenrod">
+                "In a [SITUATION], all [PROTAGONIST] wants is [OBJECTIVE]." &#8212;dramatic statement
+              </p>
+              <p>
+                Or something along those lines.
+              </p>
+            </div>
+          </Collapse>
+          <div className="Lexend-Tera pb-3">
+            {/* eslint-disable-next-line */}
+            <a href="https://guides.nanowritlabs.com/guides/PremiseBuilder/#the-disaster" target="_blank">Guide</a>
+            {/* eslint-disable-next-line */}
+            <a href="https://workshop.nanowritlabs.com/c/PremiseBuilder/dramatic-statement/" target="_blank" className="float-right">Discuss</a>
+          </div>
             <FormGroup controlId="dramaticStatement">
               <FormControl
                 value={dramaticStatement}
@@ -217,7 +378,32 @@ export default function Premise(props) {
                 onChange={e => setDramaticStatement(e.target.value)}
               />
             </FormGroup>
-          <h3>The Dramatic Question</h3>
+          <h3>
+            The Dramatic Question
+            <GoQuestion 
+              className="pale-silver float-right" 
+              onClick={() => setOpenDramaticQuestion(!openDramaticQuestion)} 
+            /> 
+          </h3>
+          <Collapse className="Lexend-Tera" in={openDramaticQuestion}>
+            <div className="pale-silver">
+              <p>
+                In the dramatic question, try to establish the opponent and the disaster in the form of a question. The fill-in-the-blank example might look something like:
+              </p>
+              <p className="text-center dark-goldenrod">
+                But can [PROTAGONIST] get [OBJECTIVE] before [OPPONENT] causes [DISASTER]?
+              </p>
+              <p>
+                I can already tell I'm going to have to rewrite that one but, for now, you hopefully get the idea.
+              </p>
+            </div>
+          </Collapse>
+          <div className="Lexend-Tera pb-3">
+            {/* eslint-disable-next-line */}
+            <a href="https://guides.nanowritlabs.com/guides/PremiseBuilder/#the-disaster" target="_blank">Guide</a>
+            {/* eslint-disable-next-line */}
+            <a href="https://workshop.nanowritlabs.com/c/PremiseBuilder/dramatic-question/" target="_blank" className="float-right">Discuss</a>
+          </div>
             <FormGroup controlId="dramaticQuestion">
               <FormControl
                 value={dramaticQuestion}
